c633caa529f7a74a277a8b46e7e125b59548af68
Eric Artzt <artzt@juniperswordplay.com>
2020-08-13 21:41:04 -0700

    successful package update

--- ui/app/controllers/settings-competencies.js
+++ ui/app/controllers/settings-competencies.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class SettingsCompetenciesController extends Controller {
   @action
-  
+  editCompetency(competency) {
+    console.log(competency);
+  }
 }
