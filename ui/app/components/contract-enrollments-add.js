9a876dee339ffb120a0f3897e081e19446c5c9f8
Eric Artzt <artzt@juniperswordplay.com>
2020-09-05 22:03:22 -0700

    provisional add enrollment

--- ui/app/components/contract-enrollments-add.js
+++ ui/app/components/contract-enrollments-add.js
@@ -1,17 +1,16 @@
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import TinyForm from './t-form';
 
-export default TinyForm.extend({
-  user: service(),
-  actions: {
-    async onSearchCandidates(name) {
-      const result = await this.user.searchStudents({ name, scope: `contract:${this.contract.id}` });
-      return result.data.map(user => ({ name: user.attributes.name, value: user.id }));
-    },
-    onChangeCandidate() {
-    },
-  },
-  save(pojo) {
-    console.log('got', pojo);
-  },
-});
+export default class ContractEnrollmentAddForm extends TinyForm {
+  @service('user') user;
+
+  @action async onSearchCandidates(name) {
+    const result = await this.user.searchStudents({ name, scope: `contract:${this.contract.id}` });
+    return result.data.map(user => ({ name: user.attributes.name, value: user.id }));
+  }
+
+  @action onChangeCandidate() {}
+
+  save = pojo => console.log('got', pojo)
+}
