e9cb96bb5bc24dbb54a56a00b9e250f08096c6ab
Eric Artzt <artzt@juniperswordplay.com>
2020-09-11 22:35:38 -0700

    validation

--- ui/app/components/contract-enrollments-add.js
+++ ui/app/components/contract-enrollments-add.js
@@ -2,16 +2,26 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import Validator from '../utils/validator';
 
 export default class ContractEnrollmentAddForm extends Component {
-  @tracked selectedUserIds = [];
+
+  validator = new Validator({
+    enrollees: [{ type: 'required', message: 'Please pick one or more enrollees' }],
+  });
+
+  @tracked enrollees = [];
 
   @tracked loading = false;
+
+  @tracked isInvalid = false;
+
+  @tracked errors = {};
 
   @service('user') user;
 
   get isDisabled() {
-    return this.selectedUserIds.length === 0 || this.loading;
+    return this.loading;
   }
 
   @action async onSearchCandidates(name) {
@@ -20,14 +30,19 @@ export default class ContractEnrollmentAddForm extends Component {
   }
 
   @action onChangeCandidate(values) {
-    this.selectedUserIds = values;
+    this.enrollees = values;
   }
 
   @action async handleSubmit() {
-    const { selectedUserIds } = this;
+    const result = this.validator.validate(this);
+
+    this.isInvalid = result.isInvalid;
+    this.errors = result.errors;
+
+    if (this.isInvalid) return;
 
     this.loading = true;
-    await this.args.addEnrollments(selectedUserIds);
+    await this.args.addEnrollments(this.enrollees);
     this.loading = false;
     this.args.onClose();
   }
