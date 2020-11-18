50d55c4f989dcb87987280c016e2df640825d7a3
Eric Artzt <artzt@juniperswordplay.com>
2020-11-17 18:53:48 -0800

    Improve contract fixtures

--- api/spec/factories/contract.rb
+++ api/spec/factories/contract.rb
@@ -7,10 +7,10 @@ FactoryBot.define do
     name { Faker::Lorem.sentence(5, 0) }
     contract_status { 1 }
     timeslots { [{ start: '8:45', end: '10:30', weekdays: '01234' }] }
-    learning_objectives { Faker::Lorem.sentence(5, 0) }
-    competencies { Faker::Lorem.sentence(5, 0) }
-    evaluation_methods { Faker::Lorem.sentence(5, 0) }
-    instructional_materials { Faker::Lorem.sentence(5, 0) }
-    location { Faker::Lorem.sentence(5, 0) }
+    learning_objectives { "Learning objectives #{Faker::Lorem.sentence(5, 0)}" }
+    competencies { "Competencies #{Faker::Lorem.sentence(5, 0)}" }
+    evaluation_methods { "Evaluation methods #{Faker::Lorem.sentence(5, 0)}" }
+    instructional_materials { "Instructional materials #{Faker::Lorem.sentence(5, 0)}" }
+    location { "Location #{Faker::Lorem.sentence(5, 0)}" }
   end
 end
