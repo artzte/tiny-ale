4ccd93a7d0dc7282c0bcd8aad700faff231d8f59
Eric Artzt <artzt@juniperswordplay.com>
2021-02-21 21:01:50 -0800

    Revert "first pass pagination for contracts page"
    This reverts commit 98b8eb33e69d6c954e4e34d1809f39c692dc0b62.

--- ui/app/controllers/contracts/index.js
+++ ui/app/controllers/contracts/index.js
@@ -1,15 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['schoolYear', 'term', 'facilitator', 'category', 'status', 'offset', 'limit'],
+  queryParams: ['schoolYear', 'term', 'facilitator', 'category', 'status'],
 
   schoolYear: '',
   term: '',
   facilitator: '',
   category: '',
   status: '',
-  offset: '0',
-  limit: '30',
 
   actions: {
     filterContracts(queryParams) {
