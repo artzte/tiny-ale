4274ae2a8f1b4655a49a83651ecc4da040e05b47
Eric Artzt <artzt@juniperswordplay.com>
2021-04-01 16:11:50 -0700

    updated notes

--- ui/app/components/status-by-student/row.js
+++ ui/app/components/status-by-student/row.js
@@ -3,9 +3,12 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { getAcademicStatusName } from '../../utils/status-utils';
+import notes from '../../mixins/notes';
 
 export default class StatusByStudentRow extends Component {
   @tracked isEditing = false;
+
+  @tracked _notes = null;
 
   @tracked _status = null;
 
@@ -38,6 +41,8 @@ export default class StatusByStudentRow extends Component {
   }
 
   get notes() {
+    if (this._notes) return this._notes;
+
     const { status, args } = this;
     const { notablesHash } = args;
     if (!(notablesHash && status)) return null;
@@ -98,7 +103,8 @@ export default class StatusByStudentRow extends Component {
       data: this.pojo,
     });
 
-    this._status = await response.data;
+    this._status = response.data;
+    this._notes = response.included.filter(json => json.type === 'note');
     this.isEditing = false;
   }
 }
