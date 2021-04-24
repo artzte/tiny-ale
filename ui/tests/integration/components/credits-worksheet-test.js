22fb6f81a83d3ea72ce606653369a193c46d7bdd
Eric Artzt <artzt@juniperswordplay.com>
2021-04-24 04:41:16 -0700

    spook

--- ui/tests/integration/components/credits-worksheet-test.js
+++ ui/tests/integration/components/credits-worksheet-test.js
@@ -39,14 +39,18 @@ module('Integration | Component | credits-worksheet', hooks => {
     });
   });
 
-  test('it renders credit assignments', async assert => {
-    await render(hbs`
+  function renderComponent() {
+    return render(hbs`
       <CreditsWorksheet
         @student={{this.student}}
         @creditAssignments={{this.creditAssignments}}
         @updateCreditAssignments={{fn this.updateCreditAssignments}}
       />
     `);
+  }
+
+  test('it renders credit assignments', async assert => {
+    await renderComponent();
 
     assert.ok(find('[data-test-credits-worksheet]'), 'worksheet table rendered');
 
@@ -58,13 +62,7 @@ module('Integration | Component | credits-worksheet', hooks => {
   });
 
   test('it renders an unapproved credit assignment with children', async assert => {
-    await render(hbs`
-      <CreditsWorksheet
-        @student={{this.student}}
-        @creditAssignments={{this.creditAssignments}}
-        @updateCreditAssignments={{fn this.updateCreditAssignments}}
-      />
-    `);
+    await renderComponent();
 
     const unbatched = creditAssignments.filter(ca => !(ca.relationships.creditTransmittalBatch.data || ca.relationships.parentCreditAssignment.data));
     const creditWithChildren = unbatched.find(ca => ca.relationships.childCreditAssignments.data.length);
@@ -93,6 +91,39 @@ module('Integration | Component | credits-worksheet', hooks => {
     assert.ok(request, 'a request was sent upon clicking approve');
     assert.matches(request.type, 'fetch', 'for a district approve request a fetch is sent');
     assert.matches(request.url, /credit-assignments\/\d+\/approve/, 'approve request was sent to correct url');
+
+    // sent updated credit assignments list
+    request = requests.shift();
+    assert.ok(request, 'a second request was sent after clicking approve');
+    assert.matches(request.type, 'updateCreditAssignments', 'updated list sent to parent component');
+  });
+
+  test('it renders an unapproved credit assignment without children', async assert => {
+    await renderComponent();
+
+    assert.ok(find('[data-test-credits-worksheet]'), 'worksheet table rendered');
+
+    const unbatched = creditAssignments.filter(ca => !(ca.relationships.creditTransmittalBatch.data || ca.relationships.parentCreditAssignment.data));
+    const unapprovedCredit = unbatched.find(ca => !(ca.attributes.districtFinalizeApprovedOn || ca.relationships.childCreditAssignments.data.length));
+
+    // testing an unapproved credit
+    const item = find(`[data-test-credit-id="${unapprovedCredit.id}"]`);
+    assert.ok(item, 'row was found for unapproved credit');
+    const checkbox = item.querySelector('input[type="checkbox"]');
+    assert.ok(checkbox, 'a checkbox was rendered');
+    assert.equal(checkbox.disabled, false, 'the checkbox is enabled');
+    assert.notOk(item.querySelector('[data-test-split-link]'), 'a split link was not rendered');
+    assert.ok(item.querySelector('[data-test-approve-link]'), 'an approve link was rendered');
+    assert.ok(item.querySelector('[data-test-district-approve]'), 'the link is shown as unapproved');
+
+    // checking approve action
+    await click(item.querySelector('[data-test-approve-link]'));
+
+    let request = requests.shift();
+    assert.ok(request, 'a request was sent upon clicking approve');
+    assert.equal(request.type, 'fetch', 'fetch sent');
+    assert.matches(request.url, `/api/students/${student.id}/credit-assignments/${unapprovedCredit.id}/approve`, 'correct endpoint targeted for district approve');
+    assert.equal(request.options.method, 'PUT', 'PUT request for district approve');
 
     // sent updated credit assignments list
     request = requests.shift();
